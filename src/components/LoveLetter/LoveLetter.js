0917a9a83ab5fdf3db6d4213b17ad04df2ad3818
divyamaddipati <21b01a12a2@svecw.edu.in>
2024-04-19 22:01:56 +0530

    added

---
+++ src/components/LoveLetter/LoveLetter.js
@@ -0,0 +1,49 @@
+import React, { useState, useRef } from 'react';
+import './LoveLetter.css';
+import audioFile from './kushi.mp3';
+
+const LoveLetter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFullSize, setIsFullSize] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleOpenLetter = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsFullSize(true);
+      // Ensuring audio play is directly a result of this user interaction
+      if (audioRef.current) {
+        audioRef.current.play()
+          .then(() => console.log("Playback succeeded"))
+          .catch(e => console.error("Playback failed:", e));
+      }
+    }, 800);
+  };
+
+  const handleCloseLetter = () => {
+    setIsFullSize(false);
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+      setIsOpen(false);
+    }, 800);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
+      <div className="flap"></div>
+      <div className="body"></div>
+      <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
+        This is a smallllll present🖤,<br />
+        <br />I loveeee youuuu My KING....Kanna Bujji Bangaram :)<br />
+         Miss u so much Darlingggg<br />
+         Loads of love and affection,<br />
+        - Divya
+      </div>
+      <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+    </div>
+  );
+};
+
+export default LoveLetter;
